3f69b80c3520c8c547874db2fbeef24c7636a6aa
janlofredy <janlofredy@gmail.com>
2023-05-06 22:00:05 +0800

    add control guide

--- pages/practice/threedy.vue
+++ pages/practice/threedy.vue
@@ -3,6 +3,12 @@
 </script>
 <template>
     <div>
+        <h1>Movement Test</h1>
+        <h3>Double Click to look</h3>
+        <h3>Esc to show Mouse</h3>
+        <h3>W,A,S,D For Movement</h3>
+        <h3>Double Space for Fly Mode</h3>
+        <h3>Space, Shift to move up and down</h3>
         <TheWindow />
     </div>
 </template>
