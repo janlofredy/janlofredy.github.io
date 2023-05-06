a89654296026bf9abfae8a73e5359423ad314e40
janlofredy <janlofredy@gmail.com>
2023-05-06 14:09:29 +0800

    add google adsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,12 @@
 export default defineNuxtConfig({
     ssr:false,
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/google-adsense',
     ],
+     'google-adsense': {
+        id: 'ca-pub-#########'
+    },
     plugins: [
         '~/plugins/aos.client.js',
     ],
@@ -17,6 +21,20 @@ export default defineNuxtConfig({
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' }
             ],
+            script: [
+                {
+                    async: true,
+                    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3178142439196865",
+                    crossorigin: "anonymous",
+                },
+            ],
         },
     },
+    publicRuntimeConfig: {
+        'google-adsense': {
+            id: process.env.GOOGLE_ADSENSE_ID,
+            test: process.env.GOOGLE_ADSENSE_TEST_MODE === 'true',
+        },
+    },
+
 })
